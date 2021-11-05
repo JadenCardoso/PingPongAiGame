c2445fd46884e401e933a49e6d39cb591231846b
JadenCardoso <90092265+JadenCardoso@users.noreply.github.com>
2021-11-05 17:54:54 +0000

    Update main.js

--- main.js
+++ main.js
@@ -23,11 +23,25 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent("canvas");
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  //poseNet.on("pose", gotPoses);
 }
 
+//function gotPoses(){
+
+//}
+
+function modelLoaded(){
+  console.log("model loaded");
+}
 
 function draw(){
-
+  image(video, 500, 600, 700, 600);
  background(0); 
 
  fill("black");
